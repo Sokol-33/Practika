1def908664fb1d037ef4018edbf2d8aae5b82b73
Данила Тяпкин <dtapkin6@gmail.com>
2023-05-04 18:12:28 +0300

    Добавьте файлы проекта.

---
+++ Temperatura/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Temperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ConvertBtnClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Цельсия",
+            "Кельвин",
+            "Фаренгейт",
+            "Ранкин",
+            "Реомюр"});
+            this.comboBox1.Location = new System.Drawing.Point(197, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Цельсия",
+            "Кельвин",
+            "Фаренгейт",
+            "Ранкин",
+            "Реомюр"});
+            this.comboBox2.Location = new System.Drawing.Point(197, 48);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Градусов по";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Перевести в градусы по";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 133);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Конвертер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Temperatura/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Temperatura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertBtnClick(object sender, EventArgs e)
+        {
+            try
+            {
+                double var = Convert.ToDouble(textBox1.Text);
+                if (comboBox1.SelectedItem.ToString() == "Цельсия")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Кельвин")
+                        MessageBox.Show($"{var} градусов по Цельсию (°C) = {var + 273.15} градусов по Кельвину (°K).");
+                    if (comboBox2.SelectedItem.ToString() == "Фаренгейт")
+                        MessageBox.Show($"{var} градусов по Цельсию (°C) ={(decimal)9 / 5 * (decimal)var + 32} градусов по Фаренгейту (°F).");
+                    if (comboBox2.SelectedItem.ToString() == "Ранкин")
+                        MessageBox.Show($"{var} градусов по Цельсию (°C) ={(decimal)(var + 273.15) * 9 / 5} градусов по Ранкину (°R).");
+                    if (comboBox2.SelectedItem.ToString() == "Реомюр")
+                        MessageBox.Show($"{var} градусов по Цельсию (°C) ={(decimal)var * 4 / 5} градусов по Реомюру (°Re).");
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Кельвин")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Цельсия")
+                        MessageBox.Show($"{var} градусов по Кельвину (°K) = {var - 273.15} градусов по Цельсию (°C).");
+                    if (comboBox2.SelectedItem.ToString() == "Фаренгейт")
+                        MessageBox.Show($"{var} градусов по Кельвину (°K) ={(decimal)var * 9 / 5 - (decimal)459.67} градусов по Фаренгейту (°F).");
+                    if (comboBox2.SelectedItem.ToString() == "Ранкин")
+                        MessageBox.Show($"{var} градусов по Кельвину (°K) ={(decimal)var * 9 / 5} градусов по Ранкину (°R).");
+                    if (comboBox2.SelectedItem.ToString() == "Реомюр")
+                        MessageBox.Show($"{var} градусов по Кельвину (°K) ={(decimal)(var - 273.15) * 4 / 5} градусов по Реомюру (°Re).");
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Фаренгейт")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Цельсия")
+                        MessageBox.Show($"{var} градусов по Фаренгейту (°F) = {(decimal)(var - 32) * 5 / 9} градусов по Цельсию (°C).");
+                    if (comboBox2.SelectedItem.ToString() == "Кельвин")
+                        MessageBox.Show($"{var} градусов по Фаренгейту (°F) ={(decimal)(var + 459.67) * 5 / 9} градусов по Кельвину (°K).");
+                    if (comboBox2.SelectedItem.ToString() == "Ранкин")
+                        MessageBox.Show($"{var} градусов по Фаренгейту (°F) ={var + 459.67} градусов по Ранкину (°R).");
+                    if (comboBox2.SelectedItem.ToString() == "Реомюр")
+                        MessageBox.Show($"{var} градусов по Фаренгейту (°F) ={(decimal)(var - 32) * 4 / 9} градусов по Реомюру (°Re).");
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Ранкин")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Цельсия")
+                        MessageBox.Show($"{var} градусов по Ранкину (°R) = {(decimal)(var - 491.67) * 5 / 9} градусов по Цельсию (°C).");
+                    if (comboBox2.SelectedItem.ToString() == "Фаренгейт")
+                        MessageBox.Show($"{var} градусов по Ранкину (°R) ={var - 459.67} градусов по Фаренгейту (°F).");
+                    if (comboBox2.SelectedItem.ToString() == "Кельвин")
+                        MessageBox.Show($"{var} градусов по Ранкину (°R) ={(decimal)var * 5 / 9} градусов по Кельвину (°K).");
+                    if (comboBox2.SelectedItem.ToString() == "Реомюр")
+                        MessageBox.Show($"{var} градусов по Ранкину (°R) ={(decimal)(var - 491.67) * 4 / 9} градусов по Реомюру (°Re).");
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Реомюр")
+                {
+                    if (comboBox2.SelectedItem.ToString() == "Цельсия")
+                        MessageBox.Show($"{var} градусов по Реомюру (°Re) = {(decimal)var * 5 / 4} градусов по Цельсию (°C).");
+                    if (comboBox2.SelectedItem.ToString() == "Фаренгейт")
+                        MessageBox.Show($"{var} градусов по Реомюру (°Re) ={(decimal)var * 9 / 4 + 32} градусов по Фаренгейту (°F).");
+                    if (comboBox2.SelectedItem.ToString() == "Кельвин")
+                        MessageBox.Show($"{var} градусов по Реомюру (°Re) ={(decimal)var * 5 / 4 + (decimal)273.15} градусов по Кельвину (°K).");
+                    if (comboBox2.SelectedItem.ToString() == "Ранкин")
+                        MessageBox.Show($"{var} градусов по Реомюру (°Re) ={(decimal)var * 9 / 4 + (decimal)491.67} градусов по Ранкину (°R).");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некорректный ввод.");
+            }
+            catch (NotFiniteNumberException)
+            {
+                MessageBox.Show("Некорректный ввод.");
+            }
+        }
+    }
+}
